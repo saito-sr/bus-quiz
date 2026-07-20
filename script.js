2af7ade6eace9e4839068df5340380bc8d8d572f
saito-sr <xt7nw6@bma.biglobe.ne.jp>
2026-07-20 12:18:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ★ Google Apps Script の Web API URL を貼る
-const API_URL = "https://script.google.com/macros/library/d/1nBosUmxpmmk9loRyawd-CmAJIlCChqEiPzmR3ftIfO_84hENlYQF7niE/2";
+const API_URL = "https://script.google.com/macros/s/AKfycbxPcfF58pk3mQuC-u5JoRVLomDM1i0r6lbqGwCJjAuJkEfW0tZtsw4mLVBdQXQQecRwUw/exec";
 
 let username = "";
 let current = 0;
